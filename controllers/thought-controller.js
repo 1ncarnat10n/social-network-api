44c5c9d37c553f249bad7af2790403b8401d0b7c
BasedAtlas <tsaiwilliam888@gmail.com>
2024-01-07 19:56:10 -0800

    Added controllers and routes content

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,28 +1,56 @@
-const {User, Thought} = require('../models');
+const { User, Thought } = require('../models');
 
 module.exports = {
     // GET all thoughts 
-    async getAllThoughts(req, res){
-        try{
+    async getAllThoughts(req, res) {
+        try {
             const thoughts = await Thought.find();
             res.json(thoughts);
-        }catch(err){
+        } catch (err) {
             res.status(500).json(err);
         }
     },
 
     // GET a single thought
-    async getSingleThought(req, res){
-        try{
-            const thought = await Thought.findOne({_id:req.params.UserId})
-            .select('-__v');
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.UserId })
+            // .select('-__v');
 
-            if(!thought){
-                return res.status(404).json({message:'No thought found'})
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found' })
             }
             res.json(thought);
-        }catch (err){
+        } catch (err) {
             res.status(500).json(err);
         }
     },
+
+    // CREATE a thought
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // UPDATE a thought 
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+            if (!thought) {
+                res.status(404).json({ message: 'No thought found with this id!' });
+            }
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
 }
+
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,27 +1,54 @@
-const {User, Thought} = require('../models');
+const { User, Thought } = require('../models');
 
 module.exports = {
     // GET all users
-    async getAllUsers(req, res){
-        try{
+    async getAllUsers(req, res) {
+        try {
             const users = await User.find();
             res.json(users);
-        }catch(err) {
-            res.status(500),json(err);
+        } catch (err) {
+            res.status(500), json(err);
         }
     },
 
     // GET a single user 
-    async getSingleUser(req, res){
+    async getSingleUser(req, res) {
         try {
-          const user = await User.findOne({_id:req.params.UserId})
-          .select('-__v');
+            const user = await User.findOne({ _id: req.params.UserId })
+            // .select('-__v');
 
-          if(!user){
-            return res.status(404).json({ message: 'No user with that ID' })
-          }
-          res.json(user);
-        }catch (err) {
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' })
+            }
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // CREATE a user 
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // UPDATE a user
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+            if (!user) {
+                res.status(404).json({ message: 'No user found with this id!' });
+            }
+            res.json(user);
+        } catch (err) {
             res.status(500).json(err);
         }
     },

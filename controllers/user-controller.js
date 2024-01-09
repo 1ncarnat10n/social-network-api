6d77de2f262885df40a94c6f9593a51dea9ab5f9
BasedAtlas <tsaiwilliam888@gmail.com>
2024-01-08 18:00:02 -0800

    Fixed minor issues

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -14,7 +14,7 @@ module.exports = {
     // GET a single user 
     async getSingleUser(req, res) {
         try {
-            const user = await User.findOne({ _id: req.params._id });
+            const user = await User.findOne({ _id: req.params.userId });
 
             if (!user) {
                 return res.status(404).json({ message: 'No user with that ID' })
